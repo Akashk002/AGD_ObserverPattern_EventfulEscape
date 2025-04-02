fdc91564c2f3388a9236af5cf95ef6e811615e06
Akash <akash.katailiha120@gmail.com>
2025-04-03 00:28:41 +0530

    Create an Event Controller

---
+++ Assets/Scripts/Events/EventController.cs
@@ -0,0 +1,23 @@
+
+using System;
+
+public class EventController 
+{
+    public Action baseEvent;
+
+    public void AddListener( Action listener)
+    {
+        baseEvent += listener;
+    }
+    
+    public void RemoveListener( Action listener)
+    {
+        baseEvent -= listener;
+    }
+
+
+    public void InvokeBaseEvent()
+    {
+        baseEvent?.Invoke();
+    }
+}
